587b2aa9c1c7ea4efb6f601e9fce8feff8ee846e
Archishmaan74 <archishmaan.upa4@gmail.com>
2025-05-08 15:59:01 +0530

    implemented html, styling, playGame check and validateGuess

---
+++ js/index.js
@@ -0,0 +1,49 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+console.log(randomNumber);
+
+const guessField = document.querySelector(".guessField");
+const submit = document.querySelector(".guessSubmit");
+const previousGuess = document.querySelector(".prevGuesses");
+const remGuesses = document.querySelector(".remGuesses");
+const lowOrHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultSection");
+
+const p = document.createElement("p");
+let prevGuess = [];
+let attemptedGuess = 1;
+let playGame = true;
+
+if (playGame) {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const guess = parseInt(guessField.value);
+    console.log(guess);
+    vaildateGuess(guess);
+  });
+}
+
+const vaildateGuess = (guess) => {
+  if (isNan(guess)) {
+    alert(`Please enter a valid input!`);
+  } else if (guess < 1) {
+    alert(`Please enter value more than 1!`);
+  } else if (guess > 100) {
+    alert(`Please enter value less than 100!`);
+  } else {
+    prevGuess.push(guess);
+    if (attemptedGuess > 10) {
+      displayGuess(guess);
+      displayMessage(`Game Over! Random number was ${randomNumber}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+};
+
+const displayGuess = () => {};
+
+const displayMessage = () => {};
+
+const checkGuess = () => {};
